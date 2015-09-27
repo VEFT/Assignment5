3652a33ed2eab9923efb46afe263abcf5f66e152
Daníel Benediktsson <danielb13@ru.is>
2015-09-27 09:04:56 -0700

    flot

--- Week06/Week06/CoursesAPI.Models/EnvelopeDTO.cs
+++ Week06/Week06/CoursesAPI.Models/EnvelopeDTO.cs
@@ -1,20 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CoursesAPI.Models
+﻿namespace CoursesAPI.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class EnvelopeDTO<T>
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public T items { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public class PagingInfo
         {
+            /// <summary>
+            /// 
+            /// </summary>
             public int PageCount { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public int PageSize { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public int PageNumber { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public int TotalNumberOfItems { get; set; }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public PagingInfo Paging { get; set; }
     }
 }
